33436c8b722929a19e432e9e6c3f291efc08ccdb
Your Name <you@example.com>
2023-03-07 21:38:45 -0500

    remove pre loader for challenge screenshot

--- src/App.js
+++ src/App.js
@@ -6,20 +6,21 @@ import Attribution from './components/Attribution';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    const loaderElement = document.querySelector(".container");
-    if (loaderElement) {
-      setTimeout(() => {
-        loaderElement.remove();
-        setIsLoading(false);
-      }, 2000)
-    }
-  })
+  // This is for add a preloader, also de index.html.
+  // const [isLoading, setIsLoading] = useState(true);
+  // useEffect(() => {
+  //   const loaderElement = document.querySelector(".container");
+  //   if (loaderElement) {
+  //     setTimeout(() => {
+  //       loaderElement.remove();
+  //       setIsLoading(false);
+  //     }, 2000)
+  //   }
+  // })
   
 
   return (
-    !isLoading &&
+    // !isLoading &&
     <div className="App md:text-base text-sm font-league font-medium min-h-screen box-border
     grid lg:grid-cols-homePageLayout lg:grid-rows-homePageLayout md:grid-rows-[100vh_100vh]
     grid-cols-1 grid-rows-homePageLayoutMobile">
