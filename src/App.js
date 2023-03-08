3eb56f8ddf4f119e40e436dfd0f9d99dcd82e0cd
Your Name <you@example.com>
2023-03-07 20:22:19 -0500

    add pre loader at html file

--- src/App.js
+++ src/App.js
@@ -3,9 +3,23 @@ import './App.css';
 import HeroHeader from './components/HeroHeader';
 import About from './components/About';
 import Attribution from './components/Attribution';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const loaderElement = document.querySelector(".container");
+    if (loaderElement) {
+      setTimeout(() => {
+        loaderElement.remove();
+        setIsLoading(false);
+      }, 2000)
+    }
+  })
+  
+
   return (
+    !isLoading &&
     <div className="App md:text-base text-sm font-league font-medium min-h-screen box-border
     grid lg:grid-cols-homePageLayout lg:grid-rows-homePageLayout md:grid-rows-[100vh_100vh]
     grid-cols-1 grid-rows-homePageLayoutMobile">
