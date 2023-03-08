b34d9af60c7c917dae3f605ff31833392af0c4ff
Your Name <you@example.com>
2023-03-07 22:08:09 -0500

    add ARIA correction 2

--- src/components/HeroHeader.jsx
+++ src/components/HeroHeader.jsx
@@ -29,17 +29,17 @@ export default function HeroHeader () {
     return (
         <header className="grid md:grid-cols-headerLayout lg:grid-rows-headerLayout grid-cols-1 grid-rows-mobileHeaderLayout
         md:grid-rows-tabletHeaderLayout 
-         overflow-hidden" id="home" role={'contentinfo'}>    
+         overflow-hidden" id="home" role={'navigation'}>    
             <div className={`bg-no-repeat bg-cover bg-center relative max-w-[840px] w-full
             ${headerContent[indexSlider].mobileImage} ${headerContent[indexSlider].desktopImage}`}
        >
                 <div className="absolute md:-right-[5.75rem] xl:-right-[10.25rem] bottom-0 right-0">
                     <button className="xl:py-6 xl:px-[34px] p-4 bg-primary-black hover:bg-primary-very-dark-gray"
-                    onClick={()=> changeSlider.toLeft(indexSlider)} id='button-left'>
+                    onClick={()=> changeSlider.toLeft(indexSlider)} id='button-left' title="left">
                         <svg className="h-[14px] xl:h-full" viewBox="0 0 14 24" width="14" height="24" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg"><path d="M13 0L1 12l12 12" stroke="#FFF" fill="none" fillRule="evenodd"/></svg>
                     </button>
                     <button className="xl:p-6 xl:px-[34px] p-4 bg-primary-black hover:bg-primary-very-dark-gray"
-                    onClick={()=> changeSlider.toRight(indexSlider)} id='button-right'>
+                    onClick={()=> changeSlider.toRight(indexSlider)} id='button-right' title="right">
                         <svg className="h-[14px] xl:h-full" viewBox="0 0 14 24" width="14" height="24" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg"><path d="M1 0l12 12L1 24" stroke="#FFF" fill="none" fillRule="evenodd"/></svg>
                     </button>
                 </div>
